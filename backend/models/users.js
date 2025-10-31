748835ff0a04c6aca7f04ddd405fc769df6dbcb2
Jazmin Maia Fabrizio <164962960+Jazfabrizio@users.noreply.github.com>
2025-10-31 14:14:11 -0300

    Agrego nuevas categorias en users.js

--- backend/models/users.js
+++ backend/models/users.js
@@ -6,7 +6,11 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   clave: { type: String, required: true },
   telefono: { type: String },
-  rol: { type: String, default: "visitante" }
+  rol: { type: String, default: "visitante" },
+  direccionCalle: { type: String },
+  direccionLocalidad: { type: String },
+  direccionProvincia: { type: String },
+  direccionPais: { type: String },
 });
 
 module.exports = mongoose.model("User", userSchema, "users");
